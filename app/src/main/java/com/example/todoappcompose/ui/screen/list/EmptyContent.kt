98d6f64454673463846def54347fe93d0fcf88b0
DysnomiaBorealis <dkphobo@gmail.com>
2023-09-17 18:51:44 +0700

    refractor

--- app/src/main/java/com/example/todoappcompose/ui/screen/list/EmptyContent.kt
+++ app/src/main/java/com/example/todoappcompose/ui/screen/list/EmptyContent.kt
@@ -14,10 +14,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.todoappcompose.R
 import com.example.todoappcompose.ui.theme.MediumGray
+import com.example.todoappcompose.ui.theme.Purple500
+import com.example.todoappcompose.ui.theme.Purple700
 
 @Composable
 fun EmptyContent() {
@@ -32,12 +35,12 @@ fun EmptyContent() {
             modifier = Modifier.size(120.dp),
             painter = painterResource(id = R.drawable.ic_sad_face),
             contentDescription = stringResource(id = R.string.sad_face_icon),
-            tint = MediumGray
+            tint = Purple700
         )
         Text(
             text = stringResource(id = R.string.empty_content),
-            color = MediumGray,
-            fontWeight = FontWeight.Bold,
+            color = Purple700,
+            fontWeight = Bold,
             fontSize = MaterialTheme.typography.h6.fontSize
         )
     }
